9bf5ebdd418aa7f316c9585f25e858bba36cc98d
efseven-f7 <90851901+efseven-f7@users.noreply.github.com>
2021-09-27 10:50:11 +0200

    Add files via upload

---
+++ BusinessData.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GSILabs.BSystem;
+
+import GSILabs.BModel.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author alumno
+ */
+public class BusinessData {
+    
+    Set<Usuario> usuarios;
+    Set<Local> locales;
+    Set<Review> reviews;
+    Set<Reserva> reservas;
+
+    public BusinessData() {
+        this.usuarios = new HashSet<>();
+        this.locales = new HashSet<>();
+        this.reviews = new HashSet<>();
+        this.reservas = new HashSet<>();
+    }
+        
+}
